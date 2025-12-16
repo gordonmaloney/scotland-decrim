17e7f38de2c85b6aee2063a2ecf9257ae2dab992
gordonmaloney <gordonmaloney@gmail.com>
2025-12-16 13:33:50 +0000

    template update

--- src/Data/CampaignData.jsx
+++ src/Data/CampaignData.jsx
@@ -27,25 +27,35 @@ export const ALL_CAMPAIGNS = [
 					answer: null,
 				},
 			],
-			subject: "Sex workers urgently need your support",
+			subject: "Reject Regan’s dangerous Bill",
 			bcc: "Scotland4Decrim@proton.me",
 			template: `Dear MSPs,
 
-I’m contacting you as a constituent to ask you to support sex workers' rights, and to work to achieve the full decriminalisation of sex work in Scotland. Decriminalisation enables sex workers to work together for safety: decriminalisation saves lives.
+I’m contacting you as a constituent to ask you to support sex workers' rights, and to reject Ash Regan’s dangerous proposed Bill. 
 
 <<whyimportant>>
 
-The poverty crisis in Scotland is driving increasing numbers of people into sex work, especially women and mothers. The government needs to take urgent action to rebuild the social safety net and enable everyone to access state support, so that fewer people have to rely on sex work to meet their needs.
+Evidence shows that the Nordic Model has failed everywhere it has been implemented: sex workers in Nordic Model countries continue to report receiving fines, being arrested, being evicted from their homes or deported, as a result of doing sex work. 
 
-At the same time, the law on sex work in Scotland puts sex workers in danger, and it must change. The partial criminalisation of sex work in Scotland forces sex workers to work alone indoors, or in more isolated areas on the street, in order to avoid being fined or arrested by the police for soliciting or ‘brothel-keeping’. This puts them at increased risk of violence from clients or the police, with no one around to help keep them safe. Criminalisation of sex workers through fines or arrests pushes sex workers further into poverty, and prevents them from being able to exit sex work. There will be people in your constituency who are facing this situation. Please take action to protect them.
+Client criminalisation puts sex workers in danger, forcing them to avoid the police, and increasing the poverty, stigma, and violence they face. Client criminalisation also drives sex workers away from support services, and worsens their health outcomes.
 
-Northern Ireland and Ireland (amongst some other European countries) have implemented the "Nordic Model" on sex work, which means criminalising the purchase of sex. Some MSPs are also campaigning to try and introduce this model in Scotland. But evidence shows that the Nordic Model has failed everywhere it has been implemented: sex workers in Nordic Model countries continue to report receiving fines, being arrested, being evicted from their homes or deported, as a result of doing sex work. Client criminalisation puts sex workers in danger, forcing them to avoid the police, and increasing the poverty, stigma, and violence they face. Client criminalisation also drives sex workers away from support services, and worsens their health outcomes.
+Liz Thomson, Amnesty International UK's Scotland programme director said: "If passed into law this Bill will cause many of the same harms against sex workers in Scotland as our researchers have documented in other jurisdictions where the purchase of sex is criminalised.
 
-Not only does the Nordic Model make sex workers less safe, it is also rejected by the Scottish public. A YouGov poll of 1,088 Scottish adults, carried out for National Ugly Mugs (NUM), the UK’s sex worker victim support charity, shows that Scots firmly oppose this controversial sex work policy agenda: only 14% of those polled supported the government passing new laws to prevent people exchanging sexual services for money. Instead, 69% of Scots say the Scottish Government should focus on protecting the health and safety of sex workers. 62% also oppose the Scottish Government’s declaration that sex work always equals violence against women, with only 13% supporting this idea.
+"As Amnesty's extensive worldwide consultations and first-hand research has shown, the main driver of sex work is poverty and the route to supporting those who want to exit sex work is to ensure that their economic and social rights like access to adequate housing, food and social security are upheld.
 
-Every sex worker-led organisation in the UK is calling for the full decriminalisation of sex work. Decriminalisation would enable sex workers to work together for safety, without fearing fines, arrests or deportation. The full decriminalisation of sex work is also endorsed by organisations including Amnesty International, the World Health Organisation, the UN Working Group on Discrimination against Women and Girls, StopAIDS, and many others. Please listen to sex workers’ demands, and take action to support them.
+"We call on MSPs to oppose the bill and to ensure that the voices and experiences of sex workers are at the centre of any decision making in Scotland that impacts their lives."
 
-Please email the sex worker-led campaign Scotland for Decrim at scotland4decrim@proton.me to receive a briefing with more information, and to arrange a meeting to hear more about what sex workers want.
+Other organisations opposed to the Nordic Model include: the World Health Organisation, Human Rights Watch, National AIDS Trust, Scottish Drug Forum, and PCS Scotland.
+
+In a 2019 review commissioned by the Northern Irish Ministry of Justice, the policy of criminalising the purchasing of sexual services in Northern Ireland was shown to be an abject failure. Reported assaults against sex workers increased by 225% from 2016 – 2018. Evidence suggested that, while some clients were deterred by the law, dangerous, violent and abusive clients remained unaffected by the legislation. Nearly all workers interviewed felt that the law had increased the stigmatisation of sex workers in ways that made them more anxious and which had a negative impact on their day to day life.
+
+A 2018 systematic review of 28 years of global research found that any criminalisation of sex work (including client criminalisation) triples the likelihood of sex workers experiencing violence, and doubles their likelihood of acquiring HIV.
+
+Not only does the Nordic Model make sex workers less safe, it is also rejected by the Scottish public. A YouGov poll of 1,088 Scottish adults, carried out for National Ugly Mugs (NUM), the UK’s sex worker victim support charity, shows that Scots firmly oppose this controversial sex work policy agenda. 69% of Scots say the Scottish Government should focus on protecting the health and safety of sex workers, and providing support to people who want to leave the industry, compared to 14% who support the government passing new laws to prevent people exchanging sexual services for money. Instead, 79% think the Scottish Government should consult sex workers and sex worker-led groups when considering new laws to keep sex workers safe (7% opposed).
+
+Every sex worker-led organisation in the UK is calling for MSPs to vote against this proposed legislation. It is vital that policymaking is based on evidence rather than ideology, and does not seek to moralise – but rather, to create the best environment possible for vulnerable groups in society. In order to do so, consider sex workers’ autonomy and their ability to ask for what they know is best for them, and vote against this Bill.
+
+Please email Scotland for Decrim at scotland4decrim@proton.me to receive a briefing with more information, and to arrange a meeting to discuss this further.
 
 Thank you,
 <<yourname>>`,
